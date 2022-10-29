cafafbfcc25298b9c207254606784f5bd3fb5bd7
oyinkansola <adebayoalameen88@gmail.com>
2022-10-29 23:07:46 +0100

    Updating

--- user_data.php
+++ user_data.php
@@ -1,23 +1,25 @@
 <?php
 
-    $name = $_POST['Name'];
-    $email = $_POST['Email'];
-    $date = $_POST['Dob'];
-    $gender = $_POST['Gender'];
-    $country = $_POST['Country'];
+    if(isset($_POST['submit'])){
+        $name = $_POST['Name'];
+        $email = $_POST['Email'];
+        $date = $_POST['Dob'];
+        $gender = $_POST['Gender'];
+        $country = $_POST['Country'];
 
-    $file = './userdata.csv';
-    $data = fopen($file, 'a');
-    fwrite($data, $name );
-    fwrite($data, $email);
-    fwrite($data, $date);
-    fwrite($data, $gender );
-    fwrite($data, $country);
-    fclose($data);
+        $file = './userdata.csv';
+        $data = fopen($file, 'a');
+        fwrite($data, $name . '</br>');
+        fwrite($data, $email . '</br>');
+        fwrite($data, $date . '</br>');
+        fwrite($data, $gender . '</br>' );
+        fwrite($data, $country . '</br>');
+        fclose($data);
 
-    $done = fopen($file, 'r');
-    $result = fread($done, filesize($file));
-    print_r($result);
-    fclose($done);
+        $done = fopen($file, 'r');
+        $result = fread($done, filesize($file));
+        print_r($result);
+        fclose($done);
+}
 
 ?>
